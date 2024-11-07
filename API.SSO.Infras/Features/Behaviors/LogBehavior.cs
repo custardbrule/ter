c4781000c32246e31c28a274d97f62779a4cbe84
Hoang Cuong <custardbruleic@gmail.com>
2024-11-08 01:19:08 +0700

    update

---
+++ API.SSO.Infras/Features/Behaviors/LogBehavior.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.SSO.Infras.Features.Behaviors
+{
+    public class LogBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // TODO handle log
+                Console.WriteLine($"{request?.GetType().Name} at {DateTimeOffset.Now:dd-MM-yyyy / H:mm:ss}");
+
+                var res = await next();
+
+                Console.WriteLine($"{request?.GetType().Name} end {DateTimeOffset.Now:dd-MM-yyyy / H:mm:ss}");
+
+                return res;
+            }
+            catch (Exception)
+            {
+                // TODO handle exception
+                throw;
+            }
+        }
+    }
+}
